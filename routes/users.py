2a784f0b43612f13dde56f7748f405ed674d4d22
VishnuPS0145 <vishnu@f22labs.com>
2025-04-08 22:29:13 +0530

    use fastApi

--- routes/users.py
+++ routes/users.py
@@ -1,1 +1,9 @@
-From fastapi import FastApi
+
+from fastapi import FastAPI, Depends, HTTPException
+
+app = FastAPI()
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int, q: str = None):
+    return {"user_id": user_id, "q": q}
+
